be69f89a8a9ff346cdcf8ebc08ee4d9a7544c686
johnconion <zyonnkonionn@gmail.com>
2020-12-12 21:34:54 +0900

    hilt を試してみた

--- app/src/main/java/tokyo/crouton/chatapp/ui/ChatActivity.kt
+++ app/src/main/java/tokyo/crouton/chatapp/ui/ChatActivity.kt
@@ -3,15 +3,23 @@ package tokyo.crouton.chatapp.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 import tokyo.crouton.chatapp.R
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class ChatActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var context: Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
-        
+
+        Log.d("WASSA", context.toString())
     }
 
     companion object {
