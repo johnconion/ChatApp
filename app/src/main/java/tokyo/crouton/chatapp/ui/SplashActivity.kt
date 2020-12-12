e7d4d7d63b36dc7fe7326ef6b8971fe437303371
johnconion <zyonnkonionn@gmail.com>
2020-12-12 10:36:38 +0900

    init

---
+++ app/src/main/java/tokyo/crouton/chatapp/ui/SplashActivity.kt
@@ -0,0 +1,12 @@
+package tokyo.crouton.chatapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tokyo.crouton.chatapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
