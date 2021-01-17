623d3ed538c7d22efe0b85d81668e7d572420f71
johnconion <zyonnkonionn@gmail.com>
2021-01-17 10:53:33 +0900

    変更通知の取り方を修正

--- datasource_realm/src/main/java/tokyo/crouton/datasource_realm/RealmManager.kt
+++ datasource_realm/src/main/java/tokyo/crouton/datasource_realm/RealmManager.kt
@@ -8,11 +8,14 @@ class RealmManager {
     companion object {
         fun <T : RealmObject> createObservable(type: Class<T>): Observable<List<T>> =
             Observable.create<List<T>> { emitter ->
-                Realm.getDefaultInstance().where(type)
-                    .findAllAsync()
-                    .addChangeListener { values ->
+                Realm.getDefaultInstance()
+                    .addChangeListener { _ ->
+                        val values =
+                            Realm.getDefaultInstance()
+                                .where(type)
+                                .findAll()
                         emitter.onNext(values)
                     }
-            }
+            }.startWith(Realm.getDefaultInstance().where(type).findAll().toList())
     }
 }
