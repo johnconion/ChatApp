c270fee70e4d65679f3374b9f8670c23d1f5f199
johnconion <zyonnkonionn@gmail.com>
2021-01-20 02:06:18 +0900

    チャットの投稿時間を表示

--- component_chat/src/main/java/tokyo/crouton/component_chat/ui/ChatListMessagePostBinder.kt
+++ component_chat/src/main/java/tokyo/crouton/component_chat/ui/ChatListMessagePostBinder.kt
@@ -1,14 +1,18 @@
 package tokyo.crouton.component_chat.ui
 
+import android.content.Context
+import dagger.hilt.android.qualifiers.ActivityContext
+import tokyo.crouton.base.toTimeString
 import tokyo.crouton.domain.chat.ChatListItem
 import javax.inject.Inject
 
 class ChatListMessagePostBinder @Inject constructor(
+    @ActivityContext private val context: Context,
     private val actions: ChatListActions
 ) {
     fun bind(viewHolder: ChatListMessagePostViewHolder, item: ChatListItem.MessagePost) {
         viewHolder.messageText.text = item.message
-        viewHolder.timeText.text = "19:25" // item.postedAt
+        viewHolder.timeText.text = item.postedAt.toTimeString(context)
 
         viewHolder.backgroundView.setOnLongClickListener {
             actions.removePost()
